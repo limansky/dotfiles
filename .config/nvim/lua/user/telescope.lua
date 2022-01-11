831e703ab846ea46bd74d88432218e4dbd11b5de
Mike Limansky <mike.limansky@gmail.com>
2022-01-12 01:33:26 +0300

    Set telescope mappings

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -1,1 +1,8 @@
 require('telescope').setup()
+
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require("telescope.builtin").live_grep()<CR>]], opts)
+vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require("telescope.builtin").find_files()<CR>]], opts)
+vim.api.nvim_set_keymap('n', '<leader>fh', [[<cmd>lua require("telescope.builtin").help_tags()<CR>]], opts)
+vim.api.nvim_set_keymap('n', '<leader>fc', [[<cmd>lua require("telescope.builtin").git_bcommits()<CR>]], opts)
