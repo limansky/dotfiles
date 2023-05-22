7c1e594cf11d9c4d2bb1bb6c0e1910f06156dda6
Mikhail Limansky <mike.limansky@gmail.com>
2023-05-22 19:28:10 +0300

    Replace packer with lazy

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,44 +1,55 @@
-return require('packer').startup(function(use)
-  -- packer itself
-  use 'wbthomason/packer.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+return require('lazy').setup({
 
   -- interface
-  use 'nvim-tree/nvim-web-devicons'
-  use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
-  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
-  use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use { 'folke/trouble.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
-  use { 'nvim-lua/popup.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use { 'lukas-reineke/indent-blankline.nvim'}
+  'nvim-tree/nvim-web-devicons',
+  { 'nvim-lualine/lualine.nvim', dependencies = {'nvim-tree/nvim-web-devicons' } },
+  {'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons' },
+  { 'nvim-tree/nvim-tree.lua', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+  { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'folke/trouble.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+  { 'nvim-lua/popup.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+  'lukas-reineke/indent-blankline.nvim',
 
   -- typing
-  use { 'windwp/nvim-autopairs' }
+  'windwp/nvim-autopairs',
 
   -- languages
-  use 'lervag/vimtex'
-  use {'scalameta/nvim-metals', requires = { 'nvim-lua/plenary.nvim' } }
+  'lervag/vimtex',
+  {'scalameta/nvim-metals', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 
-  use 'numToStr/Comment.nvim'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  use 'windwp/nvim-ts-autotag'
+  'numToStr/Comment.nvim',
+  'JoosepAlviste/nvim-ts-context-commentstring',
+  'windwp/nvim-ts-autotag',
 
   -- LSP
-  use 'neovim/nvim-lspconfig' -- enable LSP
-  use 'williamboman/nvim-lsp-installer' -- install LSP servers
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  'neovim/nvim-lspconfig', -- enable LSP
+  'williamboman/nvim-lsp-installer', -- install LSP servers
+  'hrsh7th/nvim-cmp', -- Autocompletion plugin
+  'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+  'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+  'L3MON4D3/LuaSnip', -- Snippets plugin
 
   -- git
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use { 'sindrets/diffview.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'sindrets/diffview.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- colorschemes
-  -- use 'doums/darcula'
-  use 'rebelot/kanagawa.nvim'
-end)
+  -- 'doums/darcula',
+  'rebelot/kanagawa.nvim'
+})
